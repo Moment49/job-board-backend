043de7bf80fa30b0677055a5d18cb1b1abb04af5
moment49 <ibenachoelvis49@gmail.com>
2025-09-22 08:31:55 +0100

    update:project setup, swaggerui intergration, db intergation

--- job-board-backend/urls.py
+++ job-board-backend/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls'))
+]
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api/v1/', include('api.urls')),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema')
+    
 ]
